062376af70cd889f85ae7504d954ef6d222d2e3f
Yang Xuan <jumpthepig@gmail.com>
2019-07-18 22:17:22 +0800

    change 344 headers and add example outputs

--- 301_400/344.py
+++ 301_400/344.py
@@ -1,3 +1,6 @@
+# /301_400/344.py
+# 反转字符串
+
 import sys
 sys.path.append('.')
 from schema import time_it
@@ -34,3 +37,12 @@ solu.build_in_reserve(s1)
 solu.build_in_reserve(s2)
 assert ''.join(s1) == 'hello'
 assert ''.join(s2) == 'Hannah'
+
+"""
+example output:
+
+[2019-7-18 21:46:25]: [4.3910ms]tow_pointers
+[2019-7-18 21:46:25]: [2.8640ms]tow_pointers
+[2019-7-18 21:46:25]: [1.9380ms]build_in_reserve
+[2019-7-18 21:46:25]: [0.5760ms]build_in_reserve
+"""
