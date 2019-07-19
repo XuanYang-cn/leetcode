dadeb3610f0dd05f66aeb708d906c2b136ff3ace
Yang Xuan <xuan.yang@zilliz.com>
2019-07-19 19:42:16 +0800

    unstaged change

--- schema.py
+++ schema.py
@@ -1,8 +1,9 @@
 from functools import wraps
 import time
+import os
 
 
-def time_it(func):
+def time_it_old(func):
     @wraps(func)
     def inner(*args):
         date = ('{}-{}-{} {}:{}:{}').format(*time.localtime(time.time()))
@@ -13,5 +14,49 @@ def time_it(func):
             print(f'[{date}]: [{elapsed:0.4f}ms]{func.__name__} -> {_result}')
         else:
             print(f'[{date}]: [{elapsed:0.4f}ms]{func.__name__}')
+        return _result
+    return inner
+
+
+def elapsed_since(start):
+    elapsed = time.perf_counter_ns() - start
+
+    return elapsed/1000
+
+def get_process_memory():
+    process = psutil.Process(os.getpid())
+    mi = process.memory_info()
+
+    return mi.rss, mi.vms, mi.shared
+
+def format_bytes(b):
+    if abs(b) < 1000:
+        return str(b) + "b"
+    elif abs(b) < 1e6:
+        return str(round(b/1e3, 2)) + "KB"
+    elif abs(b) < 1e9:
+        return str(round(b/1e6, 2)) + "MB"
+    else:
+        return str(round(b/1e9, 2)) + "GB"
+
+
+def time_it(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        rss_b, vms_b, sm_b = get_process_memory()
+        print(rss_b)
+        t0 = time.perf_counter_ns()
+
+        _result = func(*args, **kwargs)
+
+        elapsed = elapsed_since(t0)
+        rss_a, vms_a, sm_a = get_process_memory()
+        print(rss_a)
+        if _result is not None:
+            print(f"time: {elapsed:0.4f}ms | RSS: {} | VMS: {} | SHR: {} | [{func.__name__}] -> {_result}")
+            #print(f'[{elapsed}]{func.__name__} -> {_result}')
+        else:
+            #print(f'[{date}]: [{elapsed}]{func.__name__}')
+            print(f"time: {elapsed:0.4f}ms | RSS: {} | VMS: {} | SHR: {} | [{func.__name__}]")
         return _result
     return inner
