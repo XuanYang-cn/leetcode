3d1715e24b75968dba4c3d58db1779d92d913f09
YangXuan <jumpthepig@gmail.com>
2019-07-23 10:34:52 +0800

    add resources

--- schema.py
+++ schema.py
@@ -1,5 +1,11 @@
+"""Helper decorator to measure time costs and memery costs"""
+
+__author__= "Xuan Yang (jumpthepig@gmail.com)"
+
+
 from functools import wraps
 import time
+import resource
 
 
 def time_it(func):
@@ -13,5 +19,29 @@ def time_it(func):
             print(f'[{date}]: [{elapsed:0.4f}ms]{func.__name__} -> {_result}')
         else:
             print(f'[{date}]: [{elapsed:0.4f}ms]{func.__name__}')
+        return _result
+    return inner
+
+
+def mem_time_resource(func):
+    @wraps(func)
+    def inner(*args):
+        date = ('{}-{}-{} {}:{}:{}').format(*time.localtime(time.time()))
+        rss_before, shared_before, unshared_before = resource.getrusage(resource.RUSAGE_SELF)[2:5]
+        t_before = resource.getrusage(resource.RUSAGE_SELF).ru_utime
+
+        _result = func(*args)
+
+        t_after = resource.getrusage(resource.RUSAGE_SELF).ru_utime
+        rss_after, shared_after, unshared_after = resource.getrusage(resource.RUSAGE_SELF)[2:5]
+
+        fomater = f"mem before:{rss_before:7}, mem after:{rss_after:7}, time before:{t_before:7.8f}, time after:{t_after:7.8f}"
+        print(fomater)
+        if _result:
+            pass
+            #print(f'[{date}]: [{elapsed:0.4f}ms]{func.__name__} -> {_result}')
+        else:
+            pass
+            #print(f'[{date}]: [{elapsed:0.4f}ms]{func.__name__}')
         return _result
     return inner
