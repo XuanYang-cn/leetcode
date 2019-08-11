a7200c4ad4a59257a9b0ffd86c28c2b21b6dcee4
Yang Xuan <jumpthepig@gmail.com>
2019-08-11 17:47:39 +0800

    complete 14

--- 1_100/14.py
+++ 1_100/14.py
@@ -33,7 +33,33 @@ class Solution:
                 return s1[:i]
         return s1[:i]
 
+    @time_it
+    def longestCommonPrefix1(self, strs: list) -> str:
+        '''solve short string problem
+            Time:
+                worst:
+                    O(n*m) if all strings have the same length
+                    n is the length of the strs, m is the length of th strings
+                best:
+                    O(n * minlength)
+            Space: O(1)
+        '''
+        if len(strs) <= 0 or not strs[0]:
+            return ""
+        elif len(strs) == 1:
+            return strs[0]
+
+        for i in range(len(strs[0])):
+            for item in strs:
+                if i > len(item)-1 or strs[0][i] != item[i]:
+                    return strs[0][:i]
+        return strs[0]
+
 solu = Solution()
 strs = ["flower","flow","flight"]
+strs1 = ['a', 'a']
 
 solu.longestCommonPrefix(strs)
+solu.longestCommonPrefix1(strs)
+
+solu.longestCommonPrefix1(strs1)
